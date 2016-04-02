8b57a920e41487bbf95e623a4e439d46882fc11e
Jaime5 <Jaime5@UMBC.edu>
2016-04-02 12:07:31 -0400

    loli updated, need to create a temporary port for usage

--- src/loli.py
+++ src/loli.py
@@ -1,39 +1,42 @@
-# void setup() {
-#
-#   // Begins serial data communication at 9600 bits per second
-#   Serial.begin(9600);
-# }
-#
-# void loop() {
-#
-#   char z_string[] = "Z axis coordinate is: ";
-#
-#   char y_string[] = "Y axis coordinate is: ";
-#
-#   char x_string[] = "X axis coordinate is: ";
-#
-#
-#   float zAxisValue = analogRead(A0);
-#   float yAxisValue = analogRead(A1);
-#   float xAxisValue = analogRead(A2);
-#
-#   Serial.println(z_string);
-#   Serial.println(zAxisValue);
-#   delay(1000);
-#
-#   Serial.println(y_string);
-#   Serial.println(yAxisValue);
-#   delay(1000);
-#
-#   Serial.println(x_string);
-#   Serial.println(xAxisValue);
-#   delay(1000);
-# }
 import serial
+
+# NOTE: THIS SERIAL PORT CHANGES DEPENDING YOUR COMPUTER
 portID = "/dev/tty.usbmodem1421"
 
-with serial.Serial(portID, 9600, timeout=1) as ser:
-    while True:
-        while ser.is_open:
-            line = ser.readline()
-            print line
+count = 0
+
+recordedDirections = {
+	'x-axis': [],
+	'y-axis': [],
+	'z-axis': []
+}
+
+def storingValues(valueOfAxis, nameOfValue):
+	recordedDirections[nameOfValue].append(valueOfAxis)
+
+
+currentAxis = 0
+
+if __name__ == '__main__':
+	with serial.Serial(portID, 9600, timeout=1) as ser:
+		while True:
+			while ser.is_open:
+				line = ser.readline()
+
+				try:
+					currentAxis = float(line)
+					print currentAxis
+
+				except ValueError:
+					print "Beginning Program!"
+
+				if line != 0:
+					if count == 0:
+						storingValues(currentAxis, 'x-axis')
+					elif count == 1:
+						storingValues(currentAxis, 'y-axis')
+					elif count == 2:
+						storingValues(currentAxis, 'z-axis')
+
+				count %= 3
+				count += 1

384cf503052d04cdffa7a129655501b3b091a86b
kug3lblitz <sabbir0ahmed0@gmail.com>
2016-04-02 12:26:11 -0400

    slouch calibration done

--- src/sensor/slouch/slouch.ino
+++ src/sensor/slouch/slouch.ino
@@ -1,23 +1,20 @@
-#include <TimeLib.h> // for current time
 #include <Wire.h>
 #include "ADXL345.h"
 
 int ROTARY_ANGLE_SENSOR = 0; // pin A0
-#define LED 2 // pin D2
+#define VIBRATION 2 // pin D2
 ADXL345 ACCEL; // I2C pin 0x53
 
-time_t CURRENT_TIME = now();
-
+float X_ANG = 0;
+float Z_ANG = 0;
+int CALIB = 5000;
 
 /* Defines inputs and outputs */
 
 void pins_init() {
 
-    pinMode(ROTARY_ANGLE_SENSOR, INPUT);
-    pinMode(LED, OUTPUT);
-
-    // output current time and date
-    Serial.println(year(CURRENT_TIME));
+  pinMode(ROTARY_ANGLE_SENSOR, INPUT);
+  pinMode(VIBRATION, OUTPUT);
 
 }
 
@@ -26,42 +23,47 @@ void pins_init() {
 
 void slouch() {
 
-    double x, y, z;
-    double g_in_xyz[3];
+  double x, y, z;
+  double g_in_xyz[3];
 
-    ACCEL.getAcceleration(g_in_xyz);
-    x = g_in_xyz[0];
-    y = g_in_xyz[1];
-    z = g_in_xyz[2];
+  ACCEL.getAcceleration(g_in_xyz);
+  x = g_in_xyz[0];
+  y = g_in_xyz[1];
+  z = g_in_xyz[2];
 
-    float vec_mag =  sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2));
-    float x_ang = acos(x / vec_mag) * 180 / M_PI;
-    float y_ang = acos(y / vec_mag) * 180 / M_PI;
-    float z_ang = acos(z / vec_mag) * 180 / M_PI;
+  float vec_mag =  sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2));
+  float x_ang = acos(x / vec_mag) * 180 / M_PI;
+  //    float y_ang = acos(y / vec_mag) * 180 / M_PI; // y axis
+  float z_ang = acos(z / vec_mag) * 180 / M_PI;
 
-    Serial.print("X=");
+  if (!CALIB) {
+
     Serial.print(x_ang);
-    Serial.print(" deg");
-    Serial.print(" | Y=");
-    Serial.print(y_ang);
-    Serial.print(" deg");
-    Serial.print(" | Z=");
-    Serial.print(z_ang);
-    Serial.println(" deg");
+    Serial.print(" ");
+    Serial.println(z_ang);
 
+    if (x_ang < (X_ANG - 5) and z_ang < (Z_ANG - 5)) {
 
-    // current notification sensor for testing is an LED
-    if (x_ang < 80.0) {
-
-        Serial.println("SLOUCH");
-        digitalWrite(LED, 1);
+      digitalWrite(VIBRATION, 1);
 
     } else {
-        digitalWrite(LED, 0);
+      digitalWrite(VIBRATION, 0);
     }
 
+  }
 
-    delay(100);
+  else {
+    X_ANG = x_ang;
+    Z_ANG = z_ang;
+    CALIB -= 500;
+    if (CALIB == 500) {
+      Serial.print(X_ANG);
+      Serial.print(" ");
+      Serial.println(Z_ANG);
+    }
+  }
+
+  delay(500);
 
 }
 
@@ -70,15 +72,12 @@ void slouch() {
 
 void setup() {
 
-    // current time
-    //    CURRENT_TIME = std::time(NULL);
+  // begins the serial monitor
+  Serial.begin(9600);
 
-    // begins the serial monitor
-    Serial.begin(9600);
-
-    // define the inputs and outputs
-    pins_init();
-    ACCEL.powerOn();
+  // define the inputs and outputs
+  pins_init();
+  ACCEL.powerOn();
 
 }
 
@@ -87,13 +86,13 @@ void setup() {
 
 void loop() {
 
-    // if the potentiometer > 40, switch is considered on
-    int value = analogRead(ROTARY_ANGLE_SENSOR);
+  // if the potentiometer > 40, switch is considered on
+  int value = analogRead(ROTARY_ANGLE_SENSOR);
 
-    // call if switch is on
-    if (value > 40) {
-        slouch();
-    }
+  // call if switch is on
+  if (value > 40) {
+    slouch();
+  }
 
 }
 

22a349f6b6def53742e62905cecb4705e052c6f9
kug3lblitz <sabbir0ahmed0@gmail.com>
2016-04-02 23:09:41 -0400

    i think we done

--- src/server.py
+++ src/server.py
@@ -16,35 +16,40 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-	return render_template('home.html')
+
+    return render_template('home.html')
 
 
 # This file has a button to begin the processes required
 # for beginning serialiazation
 @app.route('/play')
 def play():
-	tacocat_obj = Tacocat()
 
-	tacocat_obj.racecar()
+    tacocat_obj = Tacocat()
+
+    tacocat_obj.racecar()
+
+    return render_template('play.html')
 
 
 # Transform to get_info
 # /sloth provides a graph that provides data reading on a person's positioning
 @app.route('/success', methods=['GET', 'POST'])
 def get_info():
-	if request.method == 'GET':
-		with open('data.json') as data_file:
-			data = load(data_file)
 
-		return render_template('success.html', data=data)
+    if request.method == 'GET':
+        with open('data.json') as data_file:
+            data = load(data_file)
+
+        return render_template('success.html', data=data)
 
 
 @app.route('/sloth')
 def sloth():
-	return render_template('sloth.html')
+    return render_template('sloth.html')
 
 
 # ----------------------------- main ----------------------------- #
 
 if __name__ == "__main__":
-	app.run(debug=True)
+    app.run(debug=True)
